1bca210d9b416c43e6b309927d0541bc4fca1999
alessandropecchini99 <pecchini99@gmail.com>
2023-04-14 01:30:52 +0200

    finish

--- script.js
+++ script.js
@@ -13,24 +13,24 @@ check.addEventListener('click', function() {
 
     // calcolo prezzi
     let normal = (km.value * 0.21);                // normale
-    const minore = (ticket - (ticket * 20 / 100)); // minore
-    const over = (ticket - (ticket * 40 / 100));   // over
+    const minore = (normal - (normal * 20 / 100)); // minore
+    const over = (normal - (normal * 40 / 100));   // over
 
     // controllo sconto
     if (age == 'maggiorenne') {
         console.log(`maggiorenne`);
         document.getElementById('prezzo').innerHTML = normal.toFixed(2) + '€';
+        document.getElementById('offerta').innerHTML = 'Normale';
     }
     if (age == 'minorenne') {
         console.log(`minorenne`);
         document.getElementById('prezzo').innerHTML = minore.toFixed(2) + '€';
+        document.getElementById('offerta').innerHTML = 'Sconto 20%';
     } 
     if (age == 'over') {
         console.log(`over`);
         document.getElementById('prezzo').innerHTML = over.toFixed(2) + '€';
-    }
-    if (isNaN(age)) {
-        document.getElementById('prezzo').innerHTML = 'Error';
+        document.getElementById('offerta').innerHTML = 'Sconto 40%';
     }
 
     document.getElementById('txtname').innerHTML = user.value;
